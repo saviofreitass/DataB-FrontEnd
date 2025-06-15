effa1a036be3ea986afea5adc64e98286049778b
Joao Pedro <ppvidalsantos@gmail.com>
2025-06-14 23:52:02 -0300

    fix: 12: Corrigindo estrutura da tabela + removendo imports não utilizados

--- src/components/TableEmpregador/TableEmpregador.jsx
+++ src/components/TableEmpregador/TableEmpregador.jsx
@@ -86,6 +86,7 @@ export const TableEmpregador = () => {
                                     }}
                                 />
                             </TableCell>
+                            <TableCell></TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
